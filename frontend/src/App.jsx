8b6fb76bb1ab2ca5bec78a3a122a0f12ab967bc2
EElectriX <saptarshimondal1002@gmail.com>
2025-07-27 18:38:57 +0530

    adding bapan

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Front from './components/front/Front';
+
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <Router>
+      <Navbar />
+      <div style={{ minHeight: '80vh' }}>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/projects" element={<h1>Projects</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
+        </Routes>
       </div>
+      </Router>
+      <Front />
       <h1>Bapan Mondal</h1>
       <div className="card">
         Bapan Mondal Photography Academy
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer />
+    
+      
+      
+     
     </>
   )
 }
